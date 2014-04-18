368c50a2d06dcc8fc6c8ee61c00b210cde148558
k-hatano <kent.ruffle.mgj626@gmail.com>
2014-04-19 00:34:39 +0900

    めっちゃ汚くなる

--- src/jp/nita/ikra/FullscreenActivity.java
+++ src/jp/nita/ikra/FullscreenActivity.java
@@ -1,10 +1,13 @@
 package jp.nita.ikra;
 
 import jp.nita.ikra.util.SystemUiHider;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -15,13 +18,22 @@ import android.webkit.WebView;
 public class FullscreenActivity extends Activity {
 	
 
+	@SuppressLint("SetJavaScriptEnabled")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_fullscreen);
 		
 		WebView webView=(WebView)findViewById(R.id.webView);
-		webView.loadUrl("http://www.google.com/");
+		webView.setWebViewClient(new WebViewClient());
+		webView.getSettings().setJavaScriptEnabled(true);
+		webView.getSettings().setSupportZoom(true);
+		webView.getSettings().setSavePassword(true);
+		webView.getSettings().setBuiltInZoomControls(true);
+		webView.getSettings().setDefaultZoom(WebSettings.ZoomDensity.FAR);
+		
+		webView.getSettings().setUserAgentString("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36");
+		webView.loadUrl("https://www.icloud.com/");	
 	}
 	
 	@Override
