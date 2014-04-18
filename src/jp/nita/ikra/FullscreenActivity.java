aab23ec7227dc815717f7b9c133605ba7c52b581
kenta <kent.ruffle.mgj626@gmail.com>
2014-04-18 18:01:45 +0900

    内部ブラウザを使うには色々設定が必要だってよ

--- src/jp/nita/ikra/FullscreenActivity.java
+++ src/jp/nita/ikra/FullscreenActivity.java
@@ -1,9 +1,10 @@
 package jp.nita.ikra;
 
 import jp.nita.ikra.util.SystemUiHider;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.webkit.WebView;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -17,11 +18,17 @@ public class FullscreenActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
 		setContentView(R.layout.activity_fullscreen);
-
 		
-		
+		WebView webView=(WebView)findViewById(R.id.webView);
+		webView.loadUrl("http://www.google.com/");
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_fullscreen, menu);
+		return true;
 	}
 
 }
